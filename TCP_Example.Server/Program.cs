670ba913aa3f8c7bb62362988c4b9346eaa953ba
Starinin Andrey <starininandrey@gmail.com>
2022-11-28 20:22:48 +0300

    Update Server

--- TCP_Example.Server/Program.cs
+++ TCP_Example.Server/Program.cs
@@ -1,5 +1,18 @@
 ﻿using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Запуск сервера...");
 
 var ip = IPAddress.Parse("127.0.0.1");
 const int PORT = 8005;
 var endPoint = new IPEndPoint(ip, PORT);
+
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(endPoint);
+Console.WriteLine("Привязка сокета к ip-адресу сервера");
+
+socket.Listen(10);
+Console.WriteLine("Ожидание подключения от клиента...");
+
+var socketOperator = socket.Accept();
+Console.WriteLine($"Подключился клиент {socketOperator.RemoteEndPoint}");
