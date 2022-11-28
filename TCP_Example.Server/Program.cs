026ac7c3bb25b210ebdeb51676d7f0c807ea6c6b
Starinin Andrey <starininandrey@gmail.com>
2022-11-28 21:16:30 +0300

    Update Server

--- TCP_Example.Server/Program.cs
+++ TCP_Example.Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 Console.WriteLine("Запуск сервера...");
 
@@ -16,3 +17,33 @@ Console.WriteLine("Ожидание подключения от клиента..
 
 var socketOperator = socket.Accept();
 Console.WriteLine($"Подключился клиент {socketOperator.RemoteEndPoint}");
+
+while (true)
+{
+    var message = Recive(socketOperator);
+    Console.WriteLine($"Сообщение от пользователя: {message}");
+
+    Send("Сообщение получено", socketOperator);
+}
+
+Console.ReadKey();
+
+string Recive(Socket socket)
+{
+    var message = new StringBuilder();
+    var buffer = new byte[256];
+    var bytes = 0;
+    do
+    {
+        bytes = socket.Receive(buffer);
+        message.Append(Encoding.UTF8.GetString(buffer, 0, bytes));
+    } while (socket.Available > 0);
+
+    return message.ToString();
+}
+
+void Send(string message, Socket socket)
+{
+    var bytes = Encoding.UTF8.GetBytes(message);
+    socket.Send(bytes);
+}
